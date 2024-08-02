fb88563a69a92074fcb180d13596cf0a018ecb77
shimei-jmh <shimei.jmh@taobao.com>
2024-08-02 11:40:26 +0800

    fix speed test bug & add state show

--- client.js
+++ client.js
@@ -7,6 +7,9 @@ for (let c = 0; c < fghex.length - 1; c += 2) {
 }
 var auto_test_suc_cnt = 0;
 const randomData_tosend = new Set();
+let LogTimeSpent = 0;
+let LogQueue = [];
+var Button_timeout = 20;
 // "Connect" button handler.
 async function connect() {
   var transport;
@@ -56,8 +59,46 @@ async function connect() {
   document.forms.sending.elements.send.disabled = false;
   document.getElementById('connect').disabled = true;
   document.getElementById('autotest').disabled = false; // Enable Auto test button
+
+  Button_checker();
+
+  flushLogQueue();
+}
+
+function Button_checker() {
+  // 这里要设置默认值 因为stream的上限是不同的
+  // 需要注意 这里uni_stream发的过多会超限（约100左右）
+  document.getElementById('unidi-stream').addEventListener('change', function () {
+    if (this.value === 'unidi') {
+      document.getElementById('autotest-count').value = '64'; 
+      Button_timeout = 100;
+    }
+  });
+
+  document.getElementById('bidi-stream').addEventListener('change', function () {
+    if (this.value === 'bidi') {
+      document.getElementById('autotest-count').value = '64'; 
+      Button_timeout = 100;
+    }
+  });
+  document.getElementById('datagram').addEventListener('change', function () {
+    if (this.value === 'datagram') {
+      document.getElementById('autotest-count').value = '2048'; 
+      Button_timeout = 20;
+    }
+  });
 }
 // "Send data" button handler.
+
+async function sendDataHandler() {
+  sendData();
+
+
+  await new Promise(r => setTimeout(r, 10));
+  flushLogQueue();
+  
+}
+
 async function sendData() {
   let form = document.forms.sending.elements;
   let encoder = new TextEncoder('utf-8');
@@ -90,7 +131,7 @@ async function sendData() {
         await writer.close();
         addToEventLog(
           'Opened bidirectional stream #' + number +
-          ' with data: ' + randomData_tosend);
+          ' with data: ' + random_gen_data);
         break;
       }
     }
@@ -193,7 +234,8 @@ function validateData(receivedData, expectedData) {
   }
   if (receivedData === expectedData) auto_test_suc_cnt += 1;
 }
-function addToEventLog(text, severity = 'info') {
+
+async function addToEventLog(text, severity = 'info') {
   let log = document.getElementById('event-log');
   let mostRecentEntry = log.lastElementChild;
   let entry = document.createElement('li');
@@ -209,20 +251,40 @@ function addToEventLog(text, severity = 'info') {
   entry.innerText = `[${timestamp}] ${text}`;
 
 
-  // 根据不同的严重性级别添加不同的样式
   if (severity === 'summary') {
     entry.style.fontWeight = 'bold';
     entry.style.color = 'red';
   } else {
     entry.className = 'log-' + severity;
   }
+  // 根据不同的严重性级别添加不同的样式
 
-  log.appendChild(entry);
-  if (mostRecentEntry != null &&
-    mostRecentEntry.getBoundingClientRect().top <
-    log.getBoundingClientRect().bottom) {
-    entry.scrollIntoView();
+  LogQueue.push(entry);
+}
+
+function flushLogQueue() {
+
+  console.log('queue size = ' + LogQueue.length);
+  let log = document.getElementById('event-log');
+  if (LogQueue.length > 10) {
+    for (let i = 0; i < 5; i++) {
+      log.appendChild(LogQueue[i]);
+    }
+    let entry = document.createElement('li');
+    entry.innerText = '...';
+    log.appendChild(entry);
+    for (let i = LogQueue.length - 5; i < LogQueue.length; i++) {
+      log.appendChild(LogQueue[i]);
+    }
   }
+  else {
+    for (let entry of LogQueue) {
+      log.appendChild(entry);
+    }
+  }
+
+  LogQueue = [];
+  log.scrollTop = log.scrollHeight;
 }
 // 随机生成字符串 给输入框
 function generateRandomString(length = 10) {
@@ -237,26 +299,37 @@ function generateRandomString(length = 10) {
 }
 async function AutoTest() {
   auto_test_suc_cnt = 0;
-  randomData_tosend.clear();
   let autoTestCount = parseInt(document.getElementById('autotest-count').value, 10);
-  let record_timestamp = new Date();
+  // 清空event-log
+  // document.getElementById('event-log').innerHTML = '';
+  LogTimeSpent = 0;
+  let record_timestamp = performance.now();
   for (let i = 0; i < autoTestCount; i++) {
-    addToEventLog('Running auto test iteration ' + (i + 1));
+    await addToEventLog('Running auto test iteration ' + (i + 1), 1);
     // randomData_tosend = generateRandomString(); // Ensure generate new random data
     await sendData();
   }
-
   // 计算总共耗时
-  let end_timestamp = new Date();
-  let time_diff = end_timestamp - record_timestamp;
+  let time_diff = performance.now() - record_timestamp;
   // wait for 0.2s to check the result
-  await new Promise(r => setTimeout(r, 20));
+  await new Promise(r => setTimeout(r, 100));
+  
+  let stateLog = document.getElementById('state-log');
   if (auto_test_suc_cnt == autoTestCount) {
     addToEventLog('Auto test success! ' + auto_test_suc_cnt + '/' + autoTestCount, 'summary');
-  }
-  else {
+    stateLog.innerText = 'State: Success';
+    stateLog.style.color = 'green';
+  } else {
     addToEventLog('Auto test failed! ' + auto_test_suc_cnt + '/' + autoTestCount, 'summary');
+    stateLog.innerText = 'State: Failed';
+    stateLog.style.color = 'red';
   }
-
   addToEventLog('Total time: ' + time_diff + 'ms', 'summary');
+  flushLogQueue();
+}
+
+function clearLog() {
+  document.getElementById('event-log').innerHTML = '';
+  document.getElementById('state-log').innerText = 'State: ';
+  LogQueue = [];
 }
